f332e3401a4ee14ca67615291992db0a30d434cf
Lionael <suryalionael@gmail.com>
2026-09-09 09:28:41 -0400

    refactor: export preparation module from features package

--- src/fraudlens/features/__init__.py
+++ src/fraudlens/features/__init__.py
@@ -1,6 +1,19 @@
 """FraudLens feature engineering module."""
 
 from fraudlens.features.engineering import FeatureEngineer
+from fraudlens.features.preparation import (
+    MODEL_FEATURES,
+    prepare_features_from_dataframe,
+    prepare_features_from_transaction,
+    get_model_features,
+)
 from fraudlens.features.tester import FeatureTester
 
-__all__ = ["FeatureEngineer", "FeatureTester"]
+__all__ = [
+    "FeatureEngineer",
+    "FeatureTester",
+    "MODEL_FEATURES",
+    "prepare_features_from_dataframe",
+    "prepare_features_from_transaction",
+    "get_model_features",
+]
